8c442ddb30e83d9c84ed949cf4cd9f2a128233e2
David Som <13154667-VidderSC@users.noreply.gitlab.com>
2023-05-02 12:35:45 +0200

    código optimizado

--- reto_005.py
+++ reto_005.py
@@ -9,6 +9,7 @@ Crea un programa que se encargue de calcular el aspect ratio de una imagen a par
 from PIL import Image
 import requests
 from io import BytesIO
+import math
 
 url = 'https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png'
 
@@ -24,18 +25,20 @@ print("Height:", height)
 
 # Function to calculate the aspect ratio and reduce it to its minimum expression
 def aspect_ratio(width, height):
-    smaller = width
-    if width > height:
-        smaller = height
-
-    dividend = 0
-    divisor = 0
-
-    for i in range(2, smaller):
+    # Find the smaller of the two
+    smaller = min(width, height)
+    # Check all possible divisors (we can stop at "smaller // 2 + 1, since any
+    # larger than this would result in a smaller quotient.
+    for i in range(2, smaller // 2 + 1):
         if width % i == 0 and height % i == 0:
-            dividend = int(width / i)
-            divisor = int(height / i)
-
+            # If we find a common divisor, calculate the simplified ratio
+            gcd = math.gcd(width, height)
+            dividend, divisor = width // gcd, height // gcd
+            return f"{dividend}:{divisor}"
+    # If no common divisor is found, return the original ratio
+    gcd = math.gcd(width, height)
+    dividend = width // gcd
+    divisor = height // gcd
     return f"{dividend}:{divisor}"
 
 
