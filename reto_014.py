04d8e86b34c85aa5b10ed9d4ffb5a35ddbcd67fb
David Som <103112459+VidderSC@users.noreply.github.com>
2023-05-08 12:13:10 +0200

    reto 014 fix

---
+++ reto_014.py
@@ -0,0 +1,18 @@
+"""
+Reto #14 - ¿ES UN NÚMERO DE ARMSTRONG?
+Escribe una función que calcule si un número dado es un número de 
+Amstrong (o también llamado narcisista).
+  - Si no conoces qué es un número de Armstrong, debes buscar 
+    información al respecto.
+"""
+
+"""
+Un número es Armstrong si la suma de cada uno de sus dígitos elevados a 
+la cantidad de dígitos que tiene el número es igual al número en sí mismo.
+
+Por ejemplo, el número 153 es un número Armstrong, ya que tiene tres dígitos y 
+1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153.
+"""
+
+def is_armstrong(num):
+    pass
